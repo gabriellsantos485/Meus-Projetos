86962b5450606b23b5a898609c2809520a0bec89
gabriellsantos485 <158094898+gabriellsantos485@users.noreply.github.com>
2024-02-10 19:02:25 -0300

    Add files via upload

---
+++ Assistente.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+client = OpenAI(
+    api_key= "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"
+)
+
+
+
+def enviar_mensagem(mensagem, lista_de_mensagem=[]):
+    lista_de_mensagem.append(
+        {'role': 'user', 'content': mensagem},
+    )
+    chat_completion = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages = lista_de_mensagem,
+    )
+    return chat_completion #["choices"][0]["message"],
+
+lista_de_mensagem = []
+while True:
+    texto = input("escreva sua mensagem")
+
+    if texto == "sair":
+        break
+    else:
+        resposta = enviar_mensagem(texto, lista_de_mensagem)
+        lista_de_mensagem.append(resposta)
+        print("chatbot", resposta)
